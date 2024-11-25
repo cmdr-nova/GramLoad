33840710fd692ae013107dd5f6948aaaf29b381d
cmdr-nova <cmdr-nova@proton.me>
2024-11-24 23:49:57 -0500

    modified setup.py

--- setup.py
+++ setup.py
@@ -12,4 +12,16 @@ setup(
             'gramload=gramload.gramload:main',
         ],
     },
+    author='cmdr-nova',
+    author_email='net_run@mkultra.monster',
+    description='A simple tool for downloading entire Instagram profiles.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/cmdr-nova/gramload',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
